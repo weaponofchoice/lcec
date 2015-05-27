258b3783b782959980567499ff903a489888820b
Luc Awater <lucawater@gmail.com>
2015-05-27 13:51:52 +0200

    added bg option

--- content/themes/elements/content/text.php
+++ content/themes/elements/content/text.php
@@ -1,13 +1,14 @@
 <?php
 // Options
 $text_o_connect = get_sub_field( 'text_o_connect' );
+$text_o_background = get_sub_field( 'text_o_background' );
 
 // Content
 $text_c_caption = get_sub_field( 'text_c_caption' );
 $text_c_title = get_sub_field( 'text_c_title' );
 $text_c_text = get_sub_field( 'text_c_text' );
 ?>
-<section class="section_text fadeIn-up">
+<section class="section_text<?php if( $text_o_background ){ echo ' has_bg'; } ?>">
   <div class="section_body">
     <p><?php echo $text_c_caption; ?></p>
     <h3><?php echo $text_c_title; ?></h3>
