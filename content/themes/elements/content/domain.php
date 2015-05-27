9d2690b3a187ec1c19027985761298bb3b44558e
Luc Awater <lucawater@gmail.com>
2015-05-27 12:21:08 +0200

    added link arrows to domain

--- content/themes/elements/content/domain.php
+++ content/themes/elements/content/domain.php
@@ -1,25 +1,32 @@
 <?php
-$domain_caption = get_sub_field( 'domain_c_caption' );
-$domain_title = get_sub_field( 'domain_c_title' );
-$domain_background = get_sub_field( 'domain_c_background' );
-$domain_text = get_sub_field( 'domain_c_text' );
-$domain_link = get_sub_field( 'domain_c_link' );
-$domain_link_text = get_sub_field( 'domain_c_link_text' );
+$domain_c_background = get_sub_field( 'domain_c_background' );
+$domain_c_caption = get_sub_field( 'domain_c_caption' );
+$domain_c_title = get_sub_field( 'domain_c_title' );
+$domain_c_text = get_sub_field( 'domain_c_text' );
 ?>
 <section class="section_domain">
   <div class="section_header fadeIn-left">
-    <p><?php echo $domain_caption; ?></p>
-    <h2><?php echo $domain_title; ?></h2>
+    <p><?php echo $domain_c_caption; ?></p>
+    <h2><?php echo $domain_c_title; ?></h2>
   </div>
   
-  <div class="top" style="background:url('<?php echo $domain_background['sizes']['large']; ?>')">
-    
-  </div>
+  <div class="top" style="background:url('<?php echo $domain_c_background['sizes']['large']; ?>')"></div>
   
   <div class="bottom">
     <div>
-      <?php echo $domain_text; ?>
-      <a href="<?php echo $domain_link; ?>"><?php echo $domain_link_text; ?></a>
+      <?php echo $domain_c_text; ?>
+      
+      <?php
+      if( have_rows('domain_c_links') ):
+        while( have_rows('domain_c_links') ): the_row();
+          $domain_c_link = get_sub_field( 'domain_c_links_link' );
+          $domain_c_link_text = get_sub_field( 'domain_c_links_text' );
+          ?>
+          <a class="link-arrow" href="<?php echo $domain_c_link; ?>"><?php echo $domain_c_link_text; ?><img src="<?php echo get_template_directory_uri(); ?>/img/arrow-pri.svg"></a>
+          <?php
+        endwhile;
+      endif;
+      ?>
     </div>
   </div>
 </section>
