0019ccd0d6899b3383535ae15f471723ccc559f3
Luc Awater <lucawater@gmail.com>
2015-05-27 14:08:53 +0200

    added text block connector

--- content/themes/elements/content/text.php
+++ content/themes/elements/content/text.php
@@ -8,7 +8,7 @@ $text_c_caption = get_sub_field( 'text_c_caption' );
 $text_c_title = get_sub_field( 'text_c_title' );
 $text_c_text = get_sub_field( 'text_c_text' );
 ?>
-<section class="section_text<?php if( $text_o_background ){ echo ' has_bg'; } ?>">
+<section class="section_text<?php if( $text_o_background ){ echo ' has_bg'; } if( $text_o_connect ){ echo ' is_connected'; } ?>">
   <div class="section_body">
     <p><?php echo $text_c_caption; ?></p>
     <h3><?php echo $text_c_title; ?></h3>
