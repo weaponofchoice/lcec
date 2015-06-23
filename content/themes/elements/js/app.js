99b87a970ba415ad323b35deb4dcd061949823ed
Luc Awater <lucawater@gmail.com>
2015-06-23 12:11:56 +0200

    tweak animated scroll script

--- content/themes/elements/js/app.js
+++ content/themes/elements/js/app.js
@@ -23,7 +23,7 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top -60
+          scrollTop: target.offset().top -120
         }, 500);
         return false;
       }
